e84930934c4943468b217f8db9a47c3e7ac14073
xudusheng <597132100@qq.com>
2016-07-25 18:03:13 +0800

    no message

---
+++ React-Native-iOS/React-Native-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  React-Native-iOS
+//
+//  Created by zhengda on 16/7/25.
+//  Copyright © 2016年 zhengda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
